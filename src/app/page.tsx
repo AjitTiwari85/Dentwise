e4bcb5e62eda8a5a36f526366db33c1e8be13f44
Ajit Tiwari <at856575@gmail.com>
2026-01-13 22:10:02 +0530

    clerk auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {SignedIn, SignedOut, SignOutButton, SignUpButton} from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click Me</Button>
+  return <div>
+    <h1>Home Page</h1>
+    <SignedOut>
+      <SignUpButton mode='modal'>Sign Up</SignUpButton>
+      </SignedOut>
+    <SignedIn>
+      <SignOutButton>Logout</SignOutButton>
+    </SignedIn>
+  </div>
 }
